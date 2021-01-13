f05717ba1dd2f4b46bd4192ac2bc6cc1b9f00aea
Raman Gupta <7243222+raman325@users.noreply.github.com>
2021-01-13 13:36:27 -0600

    Add tests using custom-components/integration_blueprint#50 as base
    * add tests using https://github.com/custom-components/integration_blueprint/pull/50 as base
    
    * move tests folder to the right place
    
    * add test files to files table
    
    * try to fix
    
    * move requirements to appropriate folder
    
    * add a note to contributing about tests
    
    * add a note to contributing about tests
    
    * Update docs/guide.rst
    
    Co-authored-by: Oncleben31 <oncleben31@users.noreply.github.com>
    
    * Update {{cookiecutter.project_name}}/CONTRIBUTING.md
    
    Co-authored-by: Oncleben31 <oncleben31@users.noreply.github.com>
    
    * Update {{cookiecutter.project_name}}/tests/test_init.py
    
    Co-authored-by: Oncleben31 <oncleben31@users.noreply.github.com>
    
    * address review comments
    
    * add more comments
    
    * fix description for requirements files
    
    * add more comments
    
    * update gitignore and fix CONTRIBUTING
    
    * add dummy init file so that pytest works
    
    * Update {{cookiecutter.project_name}}/requirements_test.txt
    
    Co-authored-by: Oncleben31 <oncleben31@users.noreply.github.com>
    
    * fix import statements
    
    * Fix spelling
    
    * bypass setup in config flow
    
    * fix comment
    
    * incorporate comments that were in integration_blueprint PR
    
    * add tests to isort known_first_party
    
    Co-authored-by: Oncleben31 <oncleben31@users.noreply.github.com>
    
    Original-Commit: oncleben31/cookiecutter-homeassistant-custom-component@b5c30b5

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Cookiecutter Home Assistant Custom Component Instance integration."""
---
+++ tests/const.py
@@ -0,0 +1,4 @@
+"""Constants for Cookiecutter Home Assistant Custom Component Instance tests."""
+from custom_components.cookiecutter_homeassistant_custom_component_instance.const import CONF_PASSWORD, CONF_USERNAME
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
